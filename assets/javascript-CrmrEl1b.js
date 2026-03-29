dc5cb30e2765cf7592f5d78735b0b0b023044125
amofly <oncwnuOVHTTfMa8XDL7qjKrTzgU0@git.weixin.qq.com>
2026-03-29 08:41:26 +0800

    update

--- assets/javascript-CrmrEl1b.js
+++ assets/javascript-CrmrEl1b.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-DSCtlKua.js";import"./index-BMgwezlJ.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-Bnn-EqsG.js";import"./index-BtJI6GLg.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.54.0(7c2310116c57517348bbd868a21139f32454be22)
  * Released under the MIT license
---
+++ plugins/cn.magic-idea.resource.rocketmq/index.js
@@ -0,0 +1,166 @@
+definePlugin('@plugins/cn.magic-idea.resource.rocketmq', ['@capital/shared/inversify', '@capital/core/plugin', '@capital/core/filesystem', '@capital/core/magic-api', 'react'], (function (inversify, plugin, filesystem, magicApi, React) { 'use strict';
+
+  function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+  var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
+
+  var e=[],t=[];function n(n,r){if(n&&"undefined"!=typeof document){var a,s=!0===r.prepend?"prepend":"append",d=!0===r.singleTag,i="string"==typeof r.container?document.querySelector(r.container):document.getElementsByTagName("head")[0];if(d){var u=e.indexOf(i);-1===u&&(u=e.push(i)-1,t[u]={}),a=t[u]&&t[u][s]?t[u][s]:t[u][s]=c();}else a=c();65279===n.charCodeAt(0)&&(n=n.substring(1)),a.styleSheet?a.styleSheet.cssText+=n:a.appendChild(document.createTextNode(n));}function c(){var e=document.createElement("style");if(e.setAttribute("type","text/css"),r.attributes)for(var t=Object.keys(r.attributes),n=0;n<t.length;n++)e.setAttribute(t[n],r.attributes[t[n]]);var a="prepend"===s?"afterbegin":"beforeend";return i.insertAdjacentElement(a,e),e}}
+
+  var css = ".rocketmq-property-form {\r\n  height: 100%;\r\n  overflow: auto;\r\n}\r\n\r\n.rocketmq-property-form .form-container{\r\n  height: 100%;\r\n}\r\n\r\n.rocketmq-property-form .form-content {\r\n  display: flex;\r\n  gap: 12px;\r\n  padding-block: 6px;\r\n}\r\n\r\n.rocketmq-property-form .form-group {\r\n  display: flex;\r\n  align-items: center;\r\n  gap: 12px;\r\n}\r\n\r\n.rocketmq-property-form .form-label {\r\n  text-align: right;\r\n  min-width: 80px;\r\n}\r\n\r\n.rocketmq-property-form .form-control-wrapper {\r\n  flex: 1;\r\n}\r\n\r\n.rocketmq-property-form .param-table-section {\r\n  width: 100%;\r\n  height: calc(100% - 42px);\r\n  border-top: 1px solid var(--magic-idea-border-color);\r\n}\r\n\r\n.rocketmq-property-form .param-table-section .form-control-wrapper {\r\n  width: 100%;\r\n  height: 100%;\r\n}\r\n\r\n.rocketmq-property-form select.form-control {\r\n  cursor: pointer;\r\n}\r\n";
+  n(css,{});
+
+  const RocketMQPropertyForm = ({
+    fileData,
+    onUpdate
+  }) => {
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      onUpdate({ [name]: name === "enabled" ? e.target.checked : value });
+    };
+    return /* @__PURE__ */ React__default["default"].createElement("div", {
+      className: "rocketmq-property-form"
+    }, /* @__PURE__ */ React__default["default"].createElement("form", {
+      className: "form-container"
+    }, /* @__PURE__ */ React__default["default"].createElement("div", {
+      className: "form-content"
+    }, /* @__PURE__ */ React__default["default"].createElement("div", {
+      className: "form-group"
+    }, /* @__PURE__ */ React__default["default"].createElement("label", {
+      className: "form-label"
+    }, "\u542F\u7528"), /* @__PURE__ */ React__default["default"].createElement("div", {
+      className: "form-control-wrapper"
+    }, /* @__PURE__ */ React__default["default"].createElement("input", {
+      type: "checkbox",
+      name: "enabled",
+      className: "form-control",
+      checked: fileData.enabled,
+      onChange: handleChange
+    }))), /* @__PURE__ */ React__default["default"].createElement("div", {
+      className: "form-group"
+    }, /* @__PURE__ */ React__default["default"].createElement("label", {
+      className: "form-label"
+    }, "\u4E3B\u9898"), /* @__PURE__ */ React__default["default"].createElement("div", {
+      className: "form-control-wrapper"
+    }, /* @__PURE__ */ React__default["default"].createElement("input", {
+      type: "text",
+      name: "topic",
+      className: "form-control",
+      style: { width: 250 },
+      value: fileData.topic,
+      onChange: handleChange,
+      placeholder: "\u8BF7\u8F93\u5165Topic\u540D\u79F0"
+    }))), /* @__PURE__ */ React__default["default"].createElement("div", {
+      className: "form-group"
+    }, /* @__PURE__ */ React__default["default"].createElement("label", {
+      className: "form-label"
+    }, "\u6807\u7B7E"), /* @__PURE__ */ React__default["default"].createElement("div", {
+      className: "form-control-wrapper"
+    }, /* @__PURE__ */ React__default["default"].createElement("input", {
+      type: "text",
+      name: "tags",
+      className: "form-control",
+      style: { width: 200 },
+      value: fileData.tag || "",
+      onChange: handleChange,
+      placeholder: "\u8BF7\u8F93\u5165\u6D88\u606F\u961F\u5217\u6807\u7B7E"
+    }))), /* @__PURE__ */ React__default["default"].createElement("div", {
+      className: "form-group"
+    }, /* @__PURE__ */ React__default["default"].createElement("label", {
+      className: "form-label"
+    }, "\u540D\u79F0"), /* @__PURE__ */ React__default["default"].createElement("div", {
+      className: "form-control-wrapper"
+    }, /* @__PURE__ */ React__default["default"].createElement("input", {
+      type: "text",
+      name: "name",
+      className: "form-control",
+      style: { width: 250 },
+      value: fileData.name,
+      onChange: handleChange,
+      placeholder: "\u8BF7\u8F93\u5165\u540D\u79F0"
+    }))), /* @__PURE__ */ React__default["default"].createElement("div", {
+      className: "form-group"
+    }, /* @__PURE__ */ React__default["default"].createElement("label", {
+      className: "form-label"
+    }, "\u8DEF\u5F84"), /* @__PURE__ */ React__default["default"].createElement("div", {
+      className: "form-control-wrapper"
+    }, /* @__PURE__ */ React__default["default"].createElement("input", {
+      type: "text",
+      name: "path",
+      className: "form-control",
+      style: { width: 350 },
+      value: fileData.path || "",
+      onChange: handleChange,
+      placeholder: "\u8BF7\u8F93\u5165\u8DEF\u5F84"
+    })))), /* @__PURE__ */ React__default["default"].createElement("div", {
+      className: "param-table-section"
+    }, /* @__PURE__ */ React__default["default"].createElement("div", {
+      className: "form-control-wrapper"
+    }, /* @__PURE__ */ React__default["default"].createElement("div", {
+      style: { width: "100%", height: "100%", padding: 4, boxSizing: "border-box" }
+    }, /* @__PURE__ */ React__default["default"].createElement("textarea", {
+      className: "form-control",
+      name: "description",
+      style: { height: "100%" },
+      value: fileData.description || "",
+      onChange: handleChange,
+      placeholder: "\u8BF7\u8F93\u5165\u63CF\u8FF0\u4FE1\u606F",
+      rows: 10
+    }))))));
+  };
+
+  var __defProp$1 = Object.defineProperty;
+  var __getOwnPropDesc$1 = Object.getOwnPropertyDescriptor;
+  var __decorateClass$1 = (decorators, target, key, kind) => {
+    var result = kind > 1 ? void 0 : kind ? __getOwnPropDesc$1(target, key) : target;
+    for (var i = decorators.length - 1, decorator; i >= 0; i--)
+      if (decorator = decorators[i])
+        result = (kind ? decorator(target, key, result) : decorator(result)) || result;
+    if (kind && result)
+      __defProp$1(target, key, result);
+    return result;
+  };
+  let RocketMQPropertyProvider = class {
+    matches(uri) {
+      return uri.resourceType === "rocketmq";
+    }
+    getFormComponent(fileData, onUpdate) {
+      return React.createElement(RocketMQPropertyForm, {
+        fileData,
+        onUpdate
+      });
+    }
+  };
+  RocketMQPropertyProvider = __decorateClass$1([
+    inversify.injectable()
+  ], RocketMQPropertyProvider);
+
+  var __defProp = Object.defineProperty;
+  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+  var __decorateClass = (decorators, target, key, kind) => {
+    var result = kind > 1 ? void 0 : kind ? __getOwnPropDesc(target, key) : target;
+    for (var i = decorators.length - 1, decorator; i >= 0; i--)
+      if (decorator = decorators[i])
+        result = (kind ? decorator(target, key, result) : decorator(result)) || result;
+    if (kind && result)
+      __defProp(target, key, result);
+    return result;
+  };
+  let MagicApiRocketMQResourceWidget = class {
+    registerResource(registry) {
+      registry.registerResource({
+        type: "rocketmq",
+        label: "RocketMQ"
+      });
+    }
+  };
+  MagicApiRocketMQResourceWidget = __decorateClass([
+    inversify.injectable()
+  ], MagicApiRocketMQResourceWidget);
+  const MagicApiRocketMQResourceModule = new inversify.ContainerModule((bind) => {
+    bind(MagicApiRocketMQResourceWidget).toSelf().inSingletonScope();
+    bind(magicApi.MagicApiResourceContribution).toService(MagicApiRocketMQResourceWidget);
+    bind(filesystem.FilePropertyProvider).to(RocketMQPropertyProvider).inSingletonScope();
+  });
+  plugin.regContainerModule(MagicApiRocketMQResourceModule);
+
+}));
